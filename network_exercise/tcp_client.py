d37418b0562895f32789755788b4b0559a826e71
longy1 <longy8@mail2.sysu.edu.cn>
2021-10-08 14:11:24 +0800

    a simple tcp app

---
+++ network_exercise/tcp_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'a simple tcp client'
+
+__author__ = 'Ethan Long'
+
+from socket import *
+import time
+
+server_name = '127.0.0.1'
+server_port = 12000
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((server_name, server_port))
+
+message1 = 'A lower sentence 1.'
+message2 = 'A lower sentence 2.'
+
+client_socket.send(message1.encode())
+client_socket.send(message2.encode())
+
+response1 = client_socket.recv(1024).decode()
+response2 = client_socket.recv(1024).decode()
+
+print(f'response 1: {response1}')
+print(f'response 2: {response2}')
+
+client_socket.close()
---
+++ network_exercise/tcp_server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'a simple tcp server'
+
+__author__ = 'Ethan Long'
+
+from socket import *
+
+server_port = 12000
+
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind(('', server_port))
+server_socket.listen(1)
+
+print('Server established.')
+
+while True:
+	connect_socket, addr = server_socket.accept()
+	message = connect_socket.recv(2048).decode()
+	response = message.upper()
+	connect_socket.send(response.encode())
+	connect_socket.close()
