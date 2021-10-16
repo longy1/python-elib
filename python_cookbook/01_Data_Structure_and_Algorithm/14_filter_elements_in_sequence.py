946449757da6b5be39a3f48428ff5edff331f7c8
longy1 <longy8@mail2.sysu.edu.cn>
2021-10-17 00:42:14 +0800

    chore: add python-cookbook notes directory

---
+++ python_cookbook/01_Data_Structure_and_Algorithm/14_filter_elements_in_sequence.py
@@ -0,0 +1,20 @@
+#!/usr/bin/ python3
+# -*- coding: utf-8 -*-
+
+'filter an iter by another iter'
+
+__author__ = 'Ethan Long'
+
+addresses = [
+    '5412 N CLARK',
+    '5148 N CLARK',
+    '5800 E 58TH',
+    '2122 N CLARK',
+    '5645 N RAVENSWOOD',
+    '1060 W ADDISON',
+    '4801 N BROADWAY',
+    '1039 W GRANVILLE',
+]
+counts = [ 0, 3, 10, 4, 1, 7, 6, 1]
+
+print([x for x in zip(addresses, counts) if x[1] > 5])
