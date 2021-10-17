f53c08e0f760f7ea0aecf6725c2aa599586651a2
longy1 <longy8@mail2.sysu.edu.cn>
2021-10-17 17:13:26 +0800

    Feature: initialize builder pattern

--- design_patterns/creational_03_builder.py
+++ design_patterns/creational_03_builder.py
@@ -5,3 +5,25 @@
 
 __author__ = 'Ethan Long'
 
+from abc import ABCMeta, abstractmethod
+
+from creational_01_base_maze_class import *
+
+
+class MazeBuilder:
+
+    def __init__(self):
+        pass
+
+    def build_maze(self):
+        pass
+
+    def build_room(self, room_no):
+        pass
+
+    def build_door(self, room1, room2):
+        pass
+
+    def get_maze(self):
+        pass
+        
